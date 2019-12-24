a4baae4b4476db7389b54050bfc2b167391cc4c0
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-12-23 21:06:12 -0600

    Add ContactDetailsWindow refactor NewContactWindow

---
+++ DesktopContactsApp/DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact contact;
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+            this.contact = contact;
+
+            nameTextBox.Text = contact.Name;
+            phoneNumberTextBox.Text = contact.Phone;
+            emailTextBox.Text = contact.Email;
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();  // Will be ignored if table already exists
+                connection.Delete(contact);
+            };
+
+            this.Close();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Name = nameTextBox.Text;
+            contact.Phone = phoneNumberTextBox.Text;
+            contact.Email = emailTextBox.Text;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();  // Will be ignored if table already exists
+                connection.Update(contact);
+            };
+
+            this.Close();
+        }
+    }
+}
