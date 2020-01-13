9ae5b8f0fe6c0d3f333abcf55aa1ecb352c75bf3
Patrick McNally <pmcnally@blackhillsip.com>
2020-01-13 17:39:03 -0600

    Add bindings to view. Add ViewModel

---
+++ WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged("CurrentContitions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "Test City Name"
+                };
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Test Weather Text",
+                    Temperature = new Temperature
+                    {
+                        Imperial = new UnitValue
+                        {
+                            Value = 32
+                        }
+                    }
+                };
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
